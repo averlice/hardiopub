a33266d60b36d9d76b6c425c3927bc79b013a03c
Molitvan <82037155+Molitvan@users.noreply.github.com>
2026-08-04 10:24:55 +0200

    Notify subscribers of new uploads

--- src/lib/components/notification.svelte
+++ src/lib/components/notification.svelte
@@ -76,6 +76,15 @@
             favorited <a {href}>{(audio as any).title}</a>
             <span class="comment-date"> - {relativeTime}</span>
         </h3>
+    {:else if notification.type == NotificationType.upload && notification.targetType == NotificationTargetType.audio}
+        {@const audio = notification.target}
+        <h3>
+            <a href={notification.actor ? `/user/@${encodeURIComponent(notification.actor.name)}` : undefined}>
+                {notification.actor?.displayName}
+            </a>
+            uploaded a new audio <a {href}>{(audio as any).title}</a>
+            <span class="comment-date"> - {relativeTime}</span>
+        </h3>
     {:else if notification.type === NotificationType.system}
         <h3>
             {notification.metadata?.title || "System"}
